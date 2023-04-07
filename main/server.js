b0063614e27be907cfbe1e2cdfa84f72eaa6c491
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-04-07 23:47:02 +0200

    benchmark w cpus

--- main/server.js
+++ main/server.js
@@ -1,5 +1,6 @@
 // import app from './app.js';
 import cluster from 'cluster';
+import os from 'os';
 // if (cluster.isPrimary) {
 // }
 
@@ -10,10 +11,14 @@ if (cluster.isPrimary) {
   // Forking is the process of creating a new process that is an exact copy of the current process.
   // The new process is called a child process, and the current process is called the parent process.
   // Every child has its own thread pool, so we can use all the cores of the machine
-  cluster.fork();
   // cluster.fork();
   // cluster.fork();
   // cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+  os.cpus().forEach(() => cluster.fork());
+  // I have 12 cores, so I can fork 12 times!?!
 } else {
   // I'm a child, I'm going to act like a server and do nothing else
   import('./app.js').then((module) => {
