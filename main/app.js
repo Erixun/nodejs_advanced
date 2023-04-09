4ac0930f47f3778d3f8ccb8a4777711dacf3d1c4
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-04-09 21:40:22 +0200

    add clarifying comments & bugfixes

--- main/app.js
+++ main/app.js
@@ -1,16 +1,20 @@
 // import cluster from 'cluster';
 import express from 'express';
-const { Worker } = require('worker_threads');
+import { Worker } from 'worker_threads';
+// const { Worker } = require('worker_threads');
 
 const app = express();
 
 app.get('/', (req, res) => {
+  //Create a new worker thread for each request
   const worker = new Worker('./worker.js');
-  worker.on('message', (message) => {
-    res.send('', message);
-  });
-
+  //Start the worker by posting a message to it
   worker.postMessage('start!');
+
+  //When the worker is done, send the result to the client
+  worker.on('message', (message) => {
+    res.send(`The CPU can count to ${message}!!`);
+  });
 });
 
 app.get('/fast', (req, res) => {
--- main/worker.js
+++ main/worker.js
@@ -1,9 +1,12 @@
-const { parentPort } = require('worker_threads');
+import { parentPort } from 'worker_threads';
 
+//Listen for messages from the main thread
 parentPort.on('message', (message) => {
+  //Once a message is received, start "doing work"
   let counter = 0;
   while (counter < 1e9) {
     counter++;
   }
+  //Once the work is done, send the result back to the main thread
   parentPort.postMessage(counter);
 });
